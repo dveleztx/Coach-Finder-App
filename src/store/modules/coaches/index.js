e320ee69ffeedb1e4bee85d60bb6788d33eba259
David Velez <dveleztx@gmail.com>
2022-04-13 23:04:09 -0500

    Created coaches store and created dummy data in the coaches/index.js file.
    Created some getters to retrieve coach data.
    
    Added store to main.js as well so that it's registered for the application for use.

---
+++ src/store/modules/coaches/index.js
@@ -0,0 +1,34 @@
+import mutations from './mutations.js';
+import actions from './actions.js';
+import getters from './getters.js';
+
+export default {
+  namespaced: true,
+  state() {
+    return {
+      coaches: [
+        {
+          id: 'c1',
+          firstName: 'Rick',
+          lastName: 'Strobel',
+          areas: ['frontend', 'backend', 'career'],
+          description:
+            "I'm Rick Strobel and I've worked as a freelance web developer for years. Let's learn together!",
+          hourlyRate: 30,
+        },
+        {
+          id: 'c2',
+          firstName: 'Jacie',
+          lastName: 'Adams',
+          areas: ['frontend', 'career'],
+          description:
+            'I am Jacie and as a senior developer in a big tech company, I can help you developer your career!',
+          hourlyRate: 30,
+        },
+      ],
+    };
+  },
+  mutations,
+  actions,
+  getters,
+};
